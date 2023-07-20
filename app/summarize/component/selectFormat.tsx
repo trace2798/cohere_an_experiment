e5b173267dca5ab7c9ec40e9fde4232249477a26
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-20 16:13:45 +0530

    added content card hover

--- app/summarize/component/selectFormat.tsx
+++ app/summarize/component/selectFormat.tsx
@@ -11,6 +11,12 @@ import {
 
 import { UseFormSetValue } from "react-hook-form";
 import { Label } from "@radix-ui/react-label";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { HoverContentComponent } from "@/components/HoverContentCompoent";
 type PromptFormValues = {
   text: string;
   length: string;
@@ -34,21 +40,45 @@ export function SelectFormat({ setValue }: SelectFormatProps) {
 
   return (
     <>
-      <Select value={selectedFormat} onValueChange={handleFormatChange}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue>{selectedFormat}</SelectValue>
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Summary Format</SelectLabel>
-            {["paragraph", "bullets", "auto"].map((format) => (
-              <SelectItem key={format} value={format}>
-                {format}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+      <div className="flex justify-between w-full my-3">
+        <div className="flex items-center justify-center w-full">
+          <HoverCard openDelay={200}>
+            <HoverCardTrigger asChild>
+              <Label htmlFor="temperature">Format</Label>
+            </HoverCardTrigger>
+            <HoverCardContent
+              align="start"
+              className="w-[260px] text-sm"
+              side="left"
+            >
+              <HoverContentComponent
+                type="string"
+                defaultValue="medium"
+                options={["short", "medium", "long", "auto"]}
+                functionality="Indicates the approximate length of the summary."
+                note="If auto is selected, the best option will be picked based on the input text."
+              />
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+        <div>
+          <Select value={selectedFormat} onValueChange={handleFormatChange}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue>{selectedFormat}</SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Summary Format</SelectLabel>
+                {["paragraph", "bullets", "auto"].map((format) => (
+                  <SelectItem key={format} value={format}>
+                    {format}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
     </>
   );
 }
